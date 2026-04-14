ad2da6d5a45e3a2612ffa5bd06866707231d53fa
SantiB <151892199+SanB00@users.noreply.github.com>
2026-04-14 14:55:05 -0300

    Se extrae metodo de mostrar error

--- TP 2_ Grupo_ 18/Common.cs
+++ TP 2_ Grupo_ 18/Common.cs
@@ -11,5 +11,20 @@ namespace TP2Grupo18
         public static string eliminarEspaciosDelTexto(string texto) {
             return Regex.Replace(texto.Trim(), @"\s+", " ");
         }
+
+        public static bool esUnNroValido(string texto) {
+            return int.TryParse(texto, out int nroValidar) || nroValidar <= 0;
+        }
+
+        public static void mostrarErrorCuandoNoEsNumero(string texto, System.Web.UI.Page page) {
+            // if (!int.TryParse(texto, out int nroValidar) || nroValidar <= 0) {
+
+            int nroValidar;
+            if (!int.TryParse(texto, out nroValidar) || nroValidar <= 0) {
+                //throw new FormatException("El valor ingresado no es un número válido.");
+                page.ClientScript.RegisterStartupScript(page.GetType(), "alert",
+                    "alert('Ingrese un número válido');", true);
+            }
+        }
     }
 }
--- TP 2_ Grupo_ 18/Ejercicio 1.aspx.cs
+++ TP 2_ Grupo_ 18/Ejercicio 1.aspx.cs
@@ -6,34 +6,29 @@ namespace TP2Grupo18
     public partial class WebForm1 : System.Web.UI.Page
     {
         protected void btnGenerarTabla_Click(object sender, EventArgs e) {
-            int numero1, numero2;
-            if (!int.TryParse(txtCantidad1.Text, out numero1) || !int.TryParse(txtCantidad2.Text, out numero2) || numero1 <= 0 || numero2 <= 0) {
-                ClientScript.RegisterStartupScript(this.GetType(), "alert",
-                    "alert('Ingrese un número válido');", true);
-            }
-            else {
+            DataTable dt = new DataTable();
+            dt.Columns.AddRange(new DataColumn[2] {
+                new DataColumn("Producto", typeof(string)),
+                new DataColumn("Cantidad", typeof(int))
+            });
+            String strCantidad1 = Common.eliminarEspaciosDelTexto(txtCantidad1.Text);
+            String strCantidad2 = Common.eliminarEspaciosDelTexto(txtCantidad2.Text);
 
+            Common.mostrarErrorCuandoNoEsNumero(strCantidad1, this);
+            Common.mostrarErrorCuandoNoEsNumero(strCantidad2, this);
 
-                DataTable dt = new DataTable();
-                dt.Columns.AddRange(new DataColumn[2] {
-                    new DataColumn("Producto", typeof(string)),
-                    new DataColumn("Cantidad", typeof(int))
-                });
-                String strCantidad1 = Common.eliminarEspaciosDelTexto(txtCantidad1.Text);
-                String strCantidad2 = Common.eliminarEspaciosDelTexto(txtCantidad2.Text);
+            int cantidad1 = string.IsNullOrEmpty(strCantidad1) ? 0 : int.Parse(strCantidad1);
+            int cantidad2 = string.IsNullOrEmpty(strCantidad2) ? 0 : int.Parse(strCantidad2);
 
-                int cantidad1 = string.IsNullOrEmpty(strCantidad1) ? 0 : int.Parse(strCantidad1);
-                int cantidad2 = string.IsNullOrEmpty(strCantidad2) ? 0 : int.Parse(strCantidad2);
+            dt.Rows.Add(txtProducto1.Text, cantidad1);
+            dt.Rows.Add(txtProducto2.Text, cantidad2);
 
-                dt.Rows.Add(txtProducto1.Text, cantidad1);
-                dt.Rows.Add(txtProducto2.Text, cantidad2);
+            int resultadoTotal = cantidad1 + cantidad2;
+            dt.Rows.Add("TOTAL", resultadoTotal.ToString());
 
-                int resultadoTotal = cantidad1 + cantidad2;
-                dt.Rows.Add("TOTAL", resultadoTotal.ToString());
+            gvListadoProductos.DataSource = dt;
+            gvListadoProductos.DataBind();
 
-                gvListadoProductos.DataSource = dt;
-                gvListadoProductos.DataBind();
-            }
         }
     }
 }

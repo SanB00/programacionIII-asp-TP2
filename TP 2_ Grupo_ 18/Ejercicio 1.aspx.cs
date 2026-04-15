06bad05cc98d79ebf53397a9b4bc81c50599be1c
SantiB <151892199+SanB00@users.noreply.github.com>
2026-04-14 22:51:43 -0300

    En respuesta a Lautaro Nehuen Gimenez Re: TP 2 - WebForms de Lautaro Nehuen Gimenez - martes, 14 de abril de 2026, 15:17 Buenas, agrego mi segundo aporte.
    En este lo que hice fue validar que no se pueda poner numero en producto y cambie la forma en la que valida, puse un string que si esta vacio hace la tabla y si no salen el o los errores concatenados

--- TP 2_ Grupo_ 18/Ejercicio 1.aspx.cs
+++ TP 2_ Grupo_ 18/Ejercicio 1.aspx.cs
@@ -6,6 +6,8 @@ namespace TP2Grupo18
     public partial class WebForm1 : System.Web.UI.Page
     {
         protected void btnGenerarTabla_Click(object sender, EventArgs e) {
+            string msgDeErrores = String.Empty;
+
             DataTable dt = new DataTable();
             dt.Columns.AddRange(new DataColumn[2] {
                 new DataColumn("Producto", typeof(string)),
@@ -14,15 +16,19 @@ namespace TP2Grupo18
             String strCantidad1 = Common.eliminarEspaciosDelTexto(txtCantidad1.Text);
             String strCantidad2 = Common.eliminarEspaciosDelTexto(txtCantidad2.Text);
 
-            if (!Common.esUnNroValido(strCantidad1)) { mostrarMensajeEnAlerta("Ingrese un número valido para el producto 1"); return; }
-            if (!Common.esUnNroValido(strCantidad2)) { mostrarMensajeEnAlerta("Ingrese un número valido para el producto 2"); return; }
+            String strProducto1 = Common.eliminarEspaciosDelTexto(txtProducto1.Text);
+            String strProducto2 = Common.eliminarEspaciosDelTexto(txtProducto2.Text);
+
+            if (!Common.esUnNroValido(strCantidad1)) { msgDeErrores += "\n * Ingrese números válidos y mayores a 0 para el producto 1"; }
+            if (!Common.esUnNroValido(strCantidad2)) { msgDeErrores += "\n * Ingrese números válidos y mayores a 0 para el producto 2"; }
+            if (!Common.esSoloLetras(strProducto1) || string.IsNullOrEmpty(strProducto1)) { msgDeErrores += "\n * Ingrese un producto válido sin números. Revisar producto 1"; }
+            if (!Common.esSoloLetras(strProducto2) || string.IsNullOrEmpty(strProducto2)) { msgDeErrores += "\n * Ingrese un producto válido sin números. Revisar producto 2"; }
+            if (!string.IsNullOrEmpty(msgDeErrores)) { mostrarMensajeEnAlerta(msgDeErrores); return; }
 
             int cantidad1 = string.IsNullOrEmpty(strCantidad1) ? 0 : int.Parse(strCantidad1);
             int cantidad2 = string.IsNullOrEmpty(strCantidad2) ? 0 : int.Parse(strCantidad2);
-
-            dt.Rows.Add(txtProducto1.Text, cantidad1);
-            dt.Rows.Add(txtProducto2.Text, cantidad2);
-
+            dt.Rows.Add(strProducto1, cantidad1);
+            dt.Rows.Add(strProducto2, cantidad2);
             int resultadoTotal = cantidad1 + cantidad2;
             dt.Rows.Add("TOTAL", resultadoTotal.ToString());
 
@@ -31,11 +37,12 @@ namespace TP2Grupo18
         }
 
         protected void mostrarMensajeEnAlerta(string mensaje) {
-            ClientScript.RegisterStartupScript(this.GetType()
-                    , "alert"
-                    , $"alert('{mensaje}');"
-                    , true
-            );
+            string safeMessage = mensaje.Replace("'", "\\'").Replace("\n", "\\n");
+            ClientScript.RegisterStartupScript(this.GetType(),
+                "alert",
+                $"alert('{safeMessage}');",
+                true);
+
         }
     }
 }

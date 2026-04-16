83d37514707c7530775b864b4d18b7542f3a9f0a
SantiB <151892199+SanB00@users.noreply.github.com>
2026-04-16 13:47:26 -0300

    git rebase --continue

--- TP2Grupo18/Common.cs
+++ TP2Grupo18/Common.cs
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 using System.Linq;
 using System.Text.RegularExpressions;
 
@@ -18,32 +17,4 @@ namespace TP2Grupo18
             return texto.All(char.IsLetter);
         }
     }
-=======
-using System.Linq;
-using System.Text.RegularExpressions;
-
-namespace TP2Grupo18
-{
-    public class Common
-    {
-        public static string eliminarEspaciosDelTexto(string texto) {
-            return Regex.Replace(texto.Trim(), @"\s+", " ");
-        }
-
-        public static bool esUnNroValido(string texto) {
-            //texto.All(char.IsDigit)
-            return int.TryParse(texto, out int nroValidar) && nroValidar > 0;
-        }
-        public static bool esSoloLetras(string texto) {
-            return texto.All(char.IsLetter);
-        }
-        public static void mostrarMensajeEnAlerta(string mensaje, System.Web.UI.Page page) {
-            string safeMessage = mensaje.Replace("'", "\\'").Replace("\n", "\\n");
-            page.ClientScript.RegisterStartupScript(page.GetType(),
-                "alert",
-                $"alert('{safeMessage}');",
-                true);
-        }
-    }
->>>>>>> e36d4de (test "Initial commit")
 }
